4d93ec4c1f098fa8d1c0ab577fd8f03a9cfb8f78
Eduard Ardeleanu <eduard-andrei.ardeleanu@1and1.ro>
2020-07-06 13:50:51 +0300

    solution: intersection of two arrays coding problem

---
+++ edu/algorithms/array/IntersectionOfTwoArrays.java
@@ -0,0 +1,54 @@
+package edu.algorithms.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given two arrays, write a function to compute their intersection.
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2,2]
+ * Example 2:
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [4,9]
+ * Note:
+ *
+ * Each element in the result should appear as many times as it shows in both arrays.
+ * The result can be in any order.
+ */
+public class IntersectionOfTwoArrays {
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+
+        int[] intersect = intersect(nums1, nums2);
+        for (Integer i : intersect) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> intersect = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                intersect.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return intersect.stream().mapToInt(num->num).toArray();
+    }
+}
