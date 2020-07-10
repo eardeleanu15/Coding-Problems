94cfe4d4f9af2318e315d22a9cd662fbddf0a9ff
Eduard Ardeleanu <eduard-andrei.ardeleanu@1and1.ro>
2020-07-10 16:08:35 +0300

    solution: anagrams leetcode coding problem

---
+++ edu/algorithms/strings/Anagrams.java
@@ -0,0 +1,40 @@
+package edu.algorithms.strings;
+
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ */
+public class Anagrams {
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] letters = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i) - 'a']++;
+            letters[t.charAt(i) - 'a']--;
+        }
+
+        for (int letter : letters) {
+            if (letter != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
